d7ae0ebdec487f4a7f95a39dc3ed031364cc180d
Brooke Carrier <brookescarrier@gmail.com>
2026-08-13 11:27:04 -0400

    Add: meeting-schedule - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule/submission-6.py
@@ -0,0 +1,25 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        # sort by start times
+        # iterate thru, if one start is < prev end, false
+
+        if len(intervals) == 0: return True
+
+        intervals.sort(key=lambda x: x.start)
+        prev_end = intervals[0].end
+
+        for interval in intervals[1:]:
+            if interval.start < prev_end:
+                return False
+            else:
+                prev_end = interval.end
+        
+        return True
